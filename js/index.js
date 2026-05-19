8593f8d6f810a5911892fd86f003e2936d6ab653
Jacob Bridges <122827480+Jacob-Bridges@users.noreply.github.com>
2026-05-19 18:20:03 -0400

    update script tag and variable name

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 
 
 /*Add a footer element*/
-body = document.querySelector('body')
+let body = document.querySelector('body')
 body.appendChild(document.createElement('footer'))
 
 /*Insert Copyright Text in Footer*/
-today = new Date()
-thisyear = today.getFullYear()
+let today = new Date()
+let thisYear = today.getFullYear()
 
-footer = document.querySelector('footer')
-copyright = document.createElement('p')
-copyright.innerHTML = `Jacob Bridges ${thisyear}`
+let footer = document.querySelector('footer')
+let copyright = document.createElement('p')
+copyright.innerHTML = `Jacob Bridges ${thisYear}`
 footer.appendChild(copyright)
 
 /*Create List of Skills*/
@@ -21,7 +21,7 @@ let skillsSection = document.getElementById('skills')
 let skillsList = skillsSection.querySelector('ul')
 for (let i = 0; i < skills.length; i++) {
     console.log(skills[i])
-    skill = document.createElement('li')
+    let skill = document.createElement('li')
     skill.innerHTML = skills[i]
     skillsList.appendChild(skill)
 
